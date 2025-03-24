3a97f7895978e26c399922255bdfd9e8531035a5
frankiewest <frankiewest.2001@icloud.com>
2025-03-24 10:23:26 +0000

    Change of variables to direct function to add ACF local field group.

--- includes/ACF/Acf_add_boat_data.php
+++ includes/ACF/Acf_add_boat_data.php
@@ -12,8 +12,8 @@ class Acf_add_boat_data {
 
 	    error_log('MS023: Function exists check: ' . (function_exists('acf_add_local_field_group') ? 'Yes' : 'No'));
 	    error_log('MS024: Class exists check: ' . (class_exists('ACF') ? 'Yes' : 'No'));
-        
-        $fields = array(
+
+	    acf_add_local_field_group(array(
             'key' => 'group_67addc1021662',
             'title' => 'Boat Data',
             'fields' => array(
@@ -2759,36 +2759,26 @@ class Acf_add_boat_data {
                     'prepend' => '',
                     'append' => '',
                 ),
-            )
-        );
-        
-        $location = array(
-            array(
-                array(
-                    'param' => 'post_type',
-                    'operator' => '==',
-                    'value' => 'marinesync-boats',
-                ),
             ),
-        );
-
-	    $args = array(
-		    'key' => 'group_67addc1021662',
-		    'title' => 'Boat Data',
-		    'fields' => $fields,
-		    'location' => $location,
-		    'menu_order' => 0,
-		    'position' => 'normal',
-		    'style' => 'default',
-		    'label_placement' => 'top',
-		    'instruction_placement' => 'label',
-		    'hide_on_screen' => '',
-		    'active' => true,
-		    'description' => '',
-		    'show_in_rest' => 0
-	    );
-        
-        acf_add_local_field_group($args);
+            'location' => array(
+	            array(
+		            array(
+			            'param' => 'post_type',
+			            'operator' => '==',
+			            'value' => 'marinesync-boats',
+		            ),
+	            ),
+            ),
+            'menu_order' => 0,
+            'position' => 'normal',
+            'style' => 'default',
+            'label_placement' => 'top',
+            'instruction_placement' => 'label',
+            'hide_on_screen' => '',
+            'active' => true,
+            'description' => '',
+            'show_in_rest' => 0
+        ));
         
         error_log('MS022: ACF field group created successfully');
     }
