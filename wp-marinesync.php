e879d633ce345f7ce1874a0d19c4d6f53ce3ef20
frankiewest <frankiewest.2001@icloud.com>
2025-07-16 10:38:55 +0100

    Fix missing newline at end of file in wp-marinesync.php

--- wp-marinesync.php
+++ wp-marinesync.php
@@ -636,4 +636,3 @@ add_shortcode('marinesync_video', function($atts) {
 		return '<div class="' . $class . '" style="max-width:640px;margin:auto;"><video width="100%" height="360" controls preload="metadata"><source src="' . esc_url($url) . '" type="video/mp4">Your browser does not support the video tag.</video></div>';
 	}
 });
-
