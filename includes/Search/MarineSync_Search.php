b98a8e7a9bb21550e2a6b1924bdf9a71eaca3275
frankiewest <frankiewest.2001@icloud.com>
2025-08-01 15:22:20 +0100

    Fix manufacturer filter by improving slug creation and sanitization for query parameter

--- includes/Search/MarineSync_Search.php
+++ includes/Search/MarineSync_Search.php
@@ -160,15 +160,20 @@ class MarineSync_Search {
 		}
 
 		// Add manufacturer filter
-		if(isset($_GET['manufacturer']) && !empty($_GET['manufacturer'])){
-			$manufacturer = sanitize_text_field(str_replace('_', ' ', $_GET['manufacturer']));
+		if (isset($_GET['manufacturer']) && !empty($_GET['manufacturer'])) {
+			// Keep original for display
+			$manufacturer_raw = sanitize_text_field($_GET['manufacturer']);
+
+			// Create slug-safe version: replace underscores with hyphens, strip ampersands
+			$manufacturer_slug = str_replace(['_', '&'], ['-', ''], strtolower($manufacturer_raw));
+			$manufacturer_slug = sanitize_title($manufacturer_slug);
+
+			// Build the tax query using the slug
 			$tax_query[] = [
-				[
-					'taxonomy' => 'manufacturer',
-					'terms' => $manufacturer,
-					'field' => 'name',
-					'operator' => 'IN'
-				]
+				'taxonomy' => 'manufacturer',
+				'field'    => 'slug',
+				'terms'    => $manufacturer_slug,
+				'operator' => 'IN',
 			];
 		}
 
