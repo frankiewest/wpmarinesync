247babd5f29c1c6168bd1a82a438d8b2ad8d99e0
frankiewest <frankiewest.2001@icloud.com>
2025-09-04 16:40:07 +0100

    Add double line breaks for improved readability in post content

--- admin/admin-page.php
+++ admin/admin-page.php
@@ -940,6 +940,7 @@ class MarineSync_Admin_Page {
                         // Convert literal \n or actual newlines in post_content into <br />
                         $desc = str_replace("\\n", "\n", $desc);   // handle literal "\n"
                         $desc = nl2br($desc, true);                // convert to <br />
+                        $desc = str_replace('<br />', '<br /><br />', $desc);
 
                         // Gather each ACF details field (HTML), if not empty, and append
 						$acf_fields = [
